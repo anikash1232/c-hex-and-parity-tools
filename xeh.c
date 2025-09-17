cf793db9083607c11a78a62e2723294a0a86c917
anikash1232 <anirudh.vpk@gmail.com>
2025-09-16 22:00:05 -0400

    Completed Part 0: hex decoder

---
+++ xeh.c
@@ -0,0 +1,46 @@
+// PID: 730761368
+// I pledge the COMP211 honor code.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int hexCharToVal(char c) {
+    if (c >= '0' && c <= '9') {
+        return c - '0';
+    } else if (c >= 'A' && c <= 'F') {
+        return c - 'A' + 10;
+    } else if (c >= 'a' && c <= 'f') {
+        return c - 'a' + 10;
+    } else {
+        return -1;
+    }
+}
+
+int main(void) {
+    int c;
+    int highNibble = -1;
+
+    while ((c = getchar()) != EOF) {
+        if (c == '\n') {
+            continue;
+        }
+
+        int val = hexCharToVal((char)c);
+        if (val == -1) {
+            continue;
+        }
+
+        if (highNibble == -1) {
+        
+            highNibble = val;
+        } else {
+
+            int byte = (highNibble << 4) | val;
+            putchar(byte);
+            fflush(stdout);
+            highNibble = -1;
+        }
+    }
+
+    return 0;
+}
